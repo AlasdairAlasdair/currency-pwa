a782f54d377f5de58ba3d83e82877e8dc46050c5
Ali Daw <alasdair.daw@red-gate.com>
2016-09-21 10:23:46 +0100

    Get exchange data but don't really update the view.

--- scripts/app.js
+++ scripts/app.js
@@ -84,12 +84,7 @@
   // doesn't already exist, it's cloned from the template.
   app.updateForecastCard = function(data) {
     var dataLastUpdated = new Date(data.created);
-    var sunrise = data.channel.astronomy.sunrise;
-    var sunset = data.channel.astronomy.sunset;
-    var current = data.channel.item.condition;
-    var humidity = data.channel.atmosphere.humidity;
-    var wind = data.channel.wind;
-
+    
     var card = app.visibleCards[data.key];
     if (!card) {
       card = app.cardTemplate.cloneNode(true);
@@ -114,33 +109,27 @@
     }
     cardLastUpdatedElem.textContent = data.created;
 
-    card.querySelector('.description').textContent = current.text;
-    card.querySelector('.date').textContent = current.date;
-    card.querySelector('.current .icon').classList.add(app.getIconClass(current.code));
-    card.querySelector('.current .temperature .value').textContent =
-      Math.round(current.temp);
-    card.querySelector('.current .sunrise').textContent = sunrise;
-    card.querySelector('.current .sunset').textContent = sunset;
-    card.querySelector('.current .humidity').textContent =
-      Math.round(humidity) + '%';
-    card.querySelector('.current .wind .value').textContent =
-      Math.round(wind.speed);
-    card.querySelector('.current .wind .direction').textContent = wind.direction;
-    var nextDays = card.querySelectorAll('.future .oneday');
+    // card.querySelector('.description').textContent = current.text;
+    // card.querySelector('.date').textContent = current.date;
+    // card.querySelector('.current .icon').classList.add(app.getIconClass(current.code));
+    // card.querySelector('.current .temperature .value').textContent = Math.round(current.temp);
+    // card.querySelector('.current .sunrise').textContent = sunrise;
+    // card.querySelector('.current .sunset').textContent = sunset;
+    // card.querySelector('.current .humidity').textContent = Math.round(humidity) + '%';
+    // card.querySelector('.current .wind .value').textContent = Math.round(wind.speed);
+    // card.querySelector('.current .wind .direction').textContent = wind.direction;
+    // var nextDays = card.querySelectorAll('.future .oneday');
     var today = new Date();
     today = today.getDay();
     for (var i = 0; i < 7; i++) {
-      var nextDay = nextDays[i];
-      var daily = data.channel.item.forecast[i];
-      if (daily && nextDay) {
-        nextDay.querySelector('.date').textContent =
-          app.daysOfWeek[(i + today) % 7];
-        nextDay.querySelector('.icon').classList.add(app.getIconClass(daily.code));
-        nextDay.querySelector('.temp-high .value').textContent =
-          Math.round(daily.high);
-        nextDay.querySelector('.temp-low .value').textContent =
-          Math.round(daily.low);
-      }
+    //   var nextDay = nextDays[i];
+    //   var daily = data.channel.item.forecast[i];
+    //   if (daily && nextDay) {
+        // nextDay.querySelector('.date').textContent = app.daysOfWeek[(i + today) % 7];
+        // nextDay.querySelector('.icon').classList.add(app.getIconClass(daily.code));
+        // nextDay.querySelector('.temp-high .value').textContent = Math.round(daily.high);
+        // nextDay.querySelector('.temp-low .value').textContent = Math.round(daily.low);
+    //   }
     }
     if (app.isLoading) {
       app.spinner.setAttribute('hidden', true);
@@ -165,9 +154,8 @@
    * freshest data.
    */
   app.getForecast = function(key, label) {
-    var statement = 'select * from weather.forecast where woeid=' + key;
-    var url = 'https://query.yahooapis.com/v1/public/yql?format=json&q=' +
-        statement;
+    // can't use https
+    var url = 'http://api.fixer.io/latest?base=GBP&symbols=USD'
     // TODO add cache logic here
     if ('caches' in window) {
       /*
@@ -180,8 +168,10 @@
           response.json().then(function updateFromCache(json) {
             var results = json.query.results;
             results.key = key;
-            results.label = label;
+            results.label = "GBP to USD";
             results.created = json.query.created;
+            results.basecurrency = "GBP";
+            results.currency = "USD";
             app.updateForecastCard(results);
           });
         }
@@ -223,68 +213,9 @@
     localStorage.selectedCities = selectedCities;
   };
 
+  // todo fix
   app.getIconClass = function(weatherCode) {
-    // Weather codes: https://developer.yahoo.com/weather/documentation.html#codes
-    weatherCode = parseInt(weatherCode);
-    switch (weatherCode) {
-      case 25: // cold
-      case 32: // sunny
-      case 33: // fair (night)
-      case 34: // fair (day)
-      case 36: // hot
-      case 3200: // not available
-        return 'clear-day';
-      case 0: // tornado
-      case 1: // tropical storm
-      case 2: // hurricane
-      case 6: // mixed rain and sleet
-      case 8: // freezing drizzle
-      case 9: // drizzle
-      case 10: // freezing rain
-      case 11: // showers
-      case 12: // showers
-      case 17: // hail
-      case 35: // mixed rain and hail
-      case 40: // scattered showers
-        return 'rain';
-      case 3: // severe thunderstorms
-      case 4: // thunderstorms
-      case 37: // isolated thunderstorms
-      case 38: // scattered thunderstorms
-      case 39: // scattered thunderstorms (not a typo)
-      case 45: // thundershowers
-      case 47: // isolated thundershowers
-        return 'thunderstorms';
-      case 5: // mixed rain and snow
-      case 7: // mixed snow and sleet
-      case 13: // snow flurries
-      case 14: // light snow showers
-      case 16: // snow
-      case 18: // sleet
-      case 41: // heavy snow
-      case 42: // scattered snow showers
-      case 43: // heavy snow
-      case 46: // snow showers
-        return 'snow';
-      case 15: // blowing snow
-      case 19: // dust
-      case 20: // foggy
-      case 21: // haze
-      case 22: // smoky
-        return 'fog';
-      case 24: // windy
-      case 23: // blustery
-        return 'windy';
-      case 26: // cloudy
-      case 27: // mostly cloudy (night)
-      case 28: // mostly cloudy (day)
-      case 31: // clear (night)
-        return 'cloudy';
-      case 29: // partly cloudy (night)
-      case 30: // partly cloudy (day)
-      case 44: // partly cloudy
-        return 'partly-cloudy-day';
-    }
+    return 'partly-cloudy-day';
   };
 
   /*
@@ -294,37 +225,12 @@
    */
   var initialWeatherForecast = {
     key: '2459115',
-    label: 'New York, NY',
+    label: 'GBP to USD',
     created: '2016-07-22T01:00:00Z',
-    channel: {
-      astronomy: {
-        sunrise: "5:43 am",
-        sunset: "8:21 pm"
-      },
-      item: {
-        condition: {
-          text: "Windy",
-          date: "Thu, 21 Jul 2016 09:00 PM EDT",
-          temp: 56,
-          code: 24
-        },
-        forecast: [
-          {code: 44, high: 86, low: 70},
-          {code: 44, high: 94, low: 73},
-          {code: 4, high: 95, low: 78},
-          {code: 24, high: 75, low: 89},
-          {code: 24, high: 89, low: 77},
-          {code: 44, high: 92, low: 79},
-          {code: 44, high: 89, low: 77}
-        ]
-      },
-      atmosphere: {
-        humidity: 56
-      },
-      wind: {
-        speed: 25,
-        direction: 195
-      }
+    basecurrency : "GBP",
+    currency : "USD",
+    rates : {
+        USD : 1.2345
     }
   };
   // TODO uncomment line below to test app with fake data
